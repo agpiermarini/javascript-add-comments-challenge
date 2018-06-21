5979c23cd3d4896da8b78980a18a95e63d884333
agpiermarini <piermarini.andrew@gmail.com>
2018-06-21 13:18:44 -0600

    Initial commit. Complete comment challenge.

--- source/addComment.js
+++ source/addComment.js
@@ -1,3 +1,16 @@
 $(document).ready(function(){
-  // add your code here
+  $('#new_comment_button').on("click", function() {
+    $('#new_comment_button').replaceWith($('<form id="new_comment" name="new_comment"><textarea placeholder="comment" name="comment" required /><input id="authorName" name="author" placeholder="author" required /><input type="submit" value="Submit" /></form>'));
+  });
+
+  $('body').on("submit", "#new_comment", function(event) {
+    event.preventDefault();
+    var formData = $( this ).serializeArray();
+    var comment  = formData[0].value;
+    var author   = formData[1].value;
+
+    console.log(formData);
+
+    $('#comment_list').append('<li>'+`${comment}`+'<span class="author">'+author+'</span></li>');
+  });
 })
